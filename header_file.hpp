f18a974834ca97d58165b05974d1035492d994d1
efredborg <e.fredborg@hotmail.com>
2020-08-28 14:56:21 +0200

    update

--- header_file.hpp
+++ header_file.hpp
@@ -4,15 +4,23 @@
 #ifndef HEADER_FILE_HPP
 #define HEADER_FILE_HPP
 
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 class Classname {
 private:
   double gi, xi, di, ei, x0, xn, h;
+  double *m_u, *m_x, *g_tilde, *d_tilde, *u_analytic;
   int m_n;
+  ofstream m_ofile; //outfile
 
 public:
 
   void Initialize(int n);
   void Function(double f(double xi), double u_func(double xi));
+  void Write_to_file(string filename);
   //void Print();
 };
 
--- main.cpp
+++ main.cpp
@@ -4,20 +4,25 @@
 #include "header_file.hpp"
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 double f(double xi);
 double u_func(double xi);
 
+
+
 int main(int argc, char const *argv[]) {
   //Parameters
-  int n = 10;
+  int n = 1000;
+  string filename = "data.txt";
 
   //Solve integral
   Classname my_solver;
   my_solver.Initialize(n);
   my_solver.Function(f,u_func);
+  my_solver.Write_to_file(filename);
   //my_solver.Print(); //ha den imens, kanskje fjerne?
 
   return 0;

b8fa7de9e16a52c8495d21bcfabd23cce370a2c2
efredborg <e.fredborg@hotmail.com>
2020-09-04 10:30:53 +0200

    update

--- class_code.cpp
+++ class_code.cpp
@@ -6,11 +6,14 @@
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
+//kommentar
 
-void Classname::Initialize(int n){
-
+void Classname::Initialize(int n,int cond){
+  m_cond=cond;
   x0 = 0.;
   xn = 1.;
   m_n = n;
@@ -21,10 +24,11 @@ void Classname::Initialize(int n){
   b = new double[m_n];
   c = new double[m_n];
   b_tilde = new double[m_n];
-  m_v_gensol = new double[m_n];      //solution
-  m_x_gen = new double[m_n];
+  m_u_gensol = new double[m_n];      //solution
+  m_x = new double[m_n];
   v_analytic = new double[m_n];
   for (int i =0 ; i<m_n; i++){
+    m_x[i] = x0 + (i+1)*h;
     a[i]=1.;
     b[i]=-2.;
     c[i]=1.;
@@ -34,36 +38,29 @@ void Classname::Initialize(int n){
   di = -2.;
   ei = 1.;
   m_u_specsol = new double[m_n]; //allokere minne
-  m_x_spec = new double[m_n]; //deklarerer array x med lengde m_n
   g_tilde = new double[m_n];
   u_analytic = new double[m_n];
 
 
 }
 
-
-
 // General:
 void Classname::Function_general(double f(double xi), double u_func(double xi)){
-
-  b_tilde[0] = pow(h,2) * f(x0+h);
-
   for (int i = 0; i< m_n;i++){
     b_tilde[i] = pow(h,2) * f(x0+h*i);
   }
 
   //Forward part:
   for(int i = 1; i < m_n; i++){
-    m_x_gen[i] = x0 + (i+1)*h;
     b_tilde[i] = b_tilde[i] - ((b_tilde[i-1]*a[i-1]) / b[i-1]);
     b[i] = b[i] - ((c[i-1]*a[i-1])/b[i-1]);
-    v_analytic[i] = u_func(m_x_gen[i]);
+    //v_analytic[i] = u_func(m_x[i]);
   }
 
   //Backward part:
-  m_v_gensol[m_n-1] =  b_tilde[m_n-1]/b[m_n-1];
+  m_u_gensol[m_n-1] =  b_tilde[m_n-1]/b[m_n-1];
   for (int i = (m_n-2); i >= 0; i--){
-    m_v_gensol[i] = (b_tilde[i]-(c[i]*m_v_gensol[i+1]))/b[i];
+    m_u_gensol[i] = (b_tilde[i]-(c[i]*m_u_gensol[i+1]))/b[i];
   }
 
 }
@@ -74,45 +71,93 @@ void Classname::Function_special(double f(double xi), double u_func(double xi)){
   g_tilde[0] = pow(h,2)*f(x0+h); //må ha denne her fordi vi bruker f, why h, skal være x0??
 
   //Forward part:
+  u_analytic[0] = u_func(m_x[0]);
   for(int i = 1; i < m_n; i++){
-    m_x_spec[i] = x0 + (i+1)*h;
-    g_tilde[i] = pow(h,2) * f(m_x_spec[i]);
-    g_tilde[i] += (g_tilde[i-1]*(i-1) / (double)(i));
-    u_analytic[i] = u_func(m_x_spec[i]);
+    g_tilde[i] = pow(h,2) * f(m_x[i]);
+    g_tilde[i] += (g_tilde[i-1]*(i) / (double)(i+1));
+    u_analytic[i] = u_func(m_x[i]);
   }
 
   //Backward part:
   m_u_specsol[m_n-1] =  -(g_tilde[m_n-1]*m_n)/(m_n+1);
   for (int i = (m_n-2); i >= 0; i--){
-    m_u_specsol[i] = -1.* (g_tilde[i] - m_u_specsol[i+1])*i / (i+1);
+    m_u_specsol[i] = -1.* (g_tilde[i] - m_u_specsol[i+1])*(1.*(i+1)/ (i+2));
   }
 
 }
 
-
-void Classname::Write_to_file(string filename){
-
-    m_ofile.open(filename);
-    m_ofile <<'#'<< 'x' <<  "               " << "gen sol" << "         " << "spec sol" << "     "<< "analytic u" <<  endl;
-    m_ofile << m_x_spec[0] <<  "               " << m_v_gensol[0] << "     " << m_u_specsol[0] << "            "<< u_analytic[0] << endl;
-    for (int i = 1; i < m_n; i++){
-
-      m_ofile << m_x_spec[i] <<  "     " << m_v_gensol[i]<< "     " << m_u_specsol[i] << "     "<< u_analytic[i] << endl;
-    }
-    m_ofile.close();
-
-}
 
 void Classname::print_relative_error(){
   double highnumber=0.;
   double error=0.;
   for (int i=0; i<m_n;i++){
     if (u_analytic[i]!=0){
-      error=sqrt(pow((m_u_specsol[i]-u_analytic[i])/u_analytic[i],2));
+      //error=sqrt(pow((m_u_specsol[i]-u_analytic[i])/u_analytic[i],2));
+      error=abs((m_u_specsol[i]-u_analytic[i])/u_analytic[i]);
     }
     if (error > highnumber){
       highnumber=error;
+
     }
   }
   cout << "log(h)= "<< log10(h) << " log_error= " << log10(highnumber) << endl;
+}
+
+
+void Classname::lu_decomp(double f(double xi)){
+  cout<<"here"<<endl;
+  mat A = mat(m_n,m_n);
+  mat L,U;
+  vec q = vec(m_n);
+  A=A.fill(0.0);
+  A(0,0)=1.;
+  for (int i=0;i<m_n-1;i++){
+    A(i,i+1)=1.;
+    A(i,i)=-2.;
+    A(i+1,i)=1.;
+  }
+  A(m_n-1,m_n-1)=-2.;
+
+  for (int i=0;i<m_n;i++){
+    q[i]=h*h*f(x0+i*h);
+  }
+  lu(L,U,A);
+  y = solve(L,q); // solving Ly = q
+  u_LU = solve(U,y); // solving Ux = y
+}
+
+
+void Classname::Write_to_file(string filename){
+
+    m_ofile.open(filename);
+    //m_ofile <<'#'<< 'x' <<  "               " << "gen sol" << "         " << "spec sol" << "     "<< "analytic u" <<  endl;
+    //m_ofile << m_x[0] <<  "  " << m_u_gensol[0] << "     " << m_u_specsol[0] << "            "<< u_analytic[0] << endl;
+    m_ofile << setw(15) << "# x";
+    m_ofile << setw(15) << " gen sol";
+    m_ofile << setw(15) << " spec sol";
+    if (m_n<m_cond){
+      m_ofile << setw(15) << " LU decomp";
+    }
+    m_ofile << setw(15) << "analytic "<<endl;
+
+
+    m_ofile << setw(15) << setprecision(8) << m_x[0];
+    m_ofile << setw(15) << setprecision(8) << m_u_gensol[0];
+    m_ofile << setw(15) << setprecision(8) << m_u_specsol[0];
+    if (m_n<m_cond){
+      m_ofile << setw(15) << setprecision(8) << u_LU(0);
+    }
+    m_ofile << setw(15) << setprecision(8) << u_analytic[0]<<endl;
+    for (int i = 1; i < m_n; i++){
+      m_ofile << setw(15) << setprecision(8) << m_x[i];
+      m_ofile << setw(15) << setprecision(8) << m_u_gensol[i];
+      m_ofile << setw(15) << setprecision(8) << m_u_specsol[i];
+      if (m_n<m_cond){
+        m_ofile << setw(15) << setprecision(8) << u_LU(i);
+      }
+      m_ofile << setw(15) << setprecision(8) << u_analytic[i]<<endl;
+      //m_ofile << m_x[i] <<  "     " << m_u_gensol[i]<< "     " << m_u_specsol[i] << "     "<< u_analytic[i] << endl;
+    }
+    m_ofile.close();
+
 }
--- plot.py
+++ plot.py
@@ -1,20 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-def plot(filename,n):
-    x,gen,spec,u_analytic=np.transpose(np.loadtxt(filename))
-    plt.figure(figsize=(6,6));plt.title('Special algo and analytic $u(x)$, n=%i'% n)
-    #plt.plot(x,spec,c='k',ls='solid',lw=0.8,label='Numerical $u(x)$')
-    plt.plot(x,gen,c='k',ls='solid',lw=0.8,label='Numerical $u(x)$')
-    plt.plot(x,u_analytic,c='k',ls='dashed',lw=0.8,label='Analytic $u(x)$')
+filename =  'data_10.txt'
+x,gen,spec,LU,analytic=np.transpose(np.loadtxt(filename))
+def plot(n):
+    plt.figure(figsize=(6,6));plt.title('Special algo and analytic $u(x)$, n=%d'% n)
+    #plt.plot(x,spec,c='k',ls='solid',lw=0.8,label='Special $u(x)$')
+    plt.plot(x,gen,c='k',ls='dotted',lw=0.8,label='General $u(x)$')
+    #plt.plot(x,analytic,c='k',ls='dashed',lw=0.8,label='Analytic $u(x)$')
+    #plt.plot(x,LU,c='k',ls='dashdot',lw=0.8,label='LU $u(x)$')
     plt.legend()
     plt.xlabel('$x$');plt.ylabel('$u(x)$')
-    plt.savefig('%i.png' % n)
+    plt.savefig('%d.png' % n)
     plt.show()
 
-    plt.figure()
-    #plt.plot(x,gen)
-    plt.show()
+N=10
+#plot(N)
 
-n=10
-plot("data_%i.txt" % n, n)
+
+def plot_rel_errors():
+    filename="log_errors.txt"
+    n,logh,log_errors = np.transpose(np.loadtxt(filename))
+    plt.figure(figsize=(6,6));plt.title('Max relative error')
+    errors=10**(log_errors)
+    plt.plot(n,errors)
+    plt.show()
+plot_rel_errors()

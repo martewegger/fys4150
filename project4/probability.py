7dea85272fd5d25c810d7f42425c897407a1c81d
GustavE <gustav.erikstad@gmail.com>
2020-11-16 12:48:51 +0100

    finished probability.py

--- project4/probability.py
+++ project4/probability.py
@@ -1,28 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def Frequency_func(energies):
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    max = np.max(abs(energies))
-    min = -max
-    values = np.arange(min,max+1,4)
+def Frequency_func(energies, in_label):
+    max_ = np.max(energies)
+    min_ = np.min(energies)
+    values = np.arange(min_,max_+1,4)
     freq = np.zeros(len(values))
     for i,value in enumerate(values):
         freq[i] = len(np.where(energies==value)[0])
     prob = freq/len(energies)
+    expected = np.sum(values*prob)
 
-    plt.figure(figsize=(8,6))
     plt.rcParams.update({'font.size': 14})
     plt.title("Probability Distribution $P(E)$", fontsize=20)
     plt.grid(axis="y",ls="--",zorder=0)
-    plt.bar(values, prob, tick_label=values, width=2.5, zorder=2)
+    plt.bar(values, prob, width=2.5, zorder=2, label=in_label)
+    plt.axvline(expected,c="r",ls="--", label="Expectation value")
+    plt.legend(fontsize=16)
     plt.xlabel("Energies", fontsize=16)
-    # plt.savefig("Probability.png")
 
     return values, prob
-
-a = np.array((-24,-20,-12,-4,-4,-4,0,0,4,4,4,4,8,12,16,20,24))
-b = Frequency_func(a)
-
-plt.show()

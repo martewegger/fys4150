c334faca1b3200f638710c0eaf67760182f57f71
efredborg <e.fredborg@hotmail.com>
2020-11-17 17:01:34 +0100

    her

--- project4/fg.py
+++ project4/fg.py
@@ -2,9 +2,15 @@ import numpy as np
 from run import *
 import matplotlib.pyplot as plt
 from probability import *
+from astropy import constants
+def Cv_func(E_mean, E_mean_sqrd, T):
+    return 1/(constants.k_B*T**2)*(E_mean_sqrd-E_mean**2)
+
+def chi_func(M_mean, M_mean_sqrd, T):
+    return 1/(constants.k_B*T)*(M_mean_sqrd-M_mean**2)
 
 def calc_func():
-    burn_in_no = np.loadtxt('burn_in_no_v3.txt')
+    burn_in_no = np.loadtxt('burn_in_no_1e+07.txt')
     T0 = 2; T1 = 2.3
     dT = 0.05
     N = int(np.abs(T1-T0)//dT)
@@ -14,7 +20,7 @@ def calc_func():
     M_mean = np.zeros((len(L_arr),len(T_arr)))
     E_mean_sqrd = np.zeros((len(L_arr),len(T_arr)))
     M_mean_sqrd = np.zeros((len(L_arr),len(T_arr)))
-    N_cycles = 1e6
+    N_cycles = 1e7
     ylen, xlen = E_mean.shape
     L2 = xlen*ylen
     a=0
@@ -26,21 +32,51 @@ def calc_func():
             E = np.loadtxt('Energy.txt')
             M = np.loadtxt('magnetisation.txt')
             E_mean[i,j] = expectation_func(E)
-            M_mean[i,j] = expectation_func(M,type='magnetisation')
+            M_mean[i,j] = expectation_func(np.abs(M),type='magnetisation')
             E_mean_sqrd[i,j] = expectation_func(E,squared=True)
             M_mean_sqrd[i,j] = expectation_func(M, squared=True, type='magnetisation')
     np.save('E_M_E2_M2.npy', np.array((E_mean, M_mean, E_mean_sqrd, M_mean_sqrd)))
+
+
 
 def plot_func():
     T0 = 2; T1 = 2.3
     L_arr = np.array((40,60,80,100))
     T_arr = np.linspace(T0,T1,7)
     E_mean, M_mean, E_mean_sqrd, M_mean_sqrd = np.load('E_M_E2_M2.npy')
-    print(E_mean_sqrd)
     ylen, xlen = E_mean.shape
+    Cv = Cv_func(E_mean, E_mean_sqrd, T[np.newaxis])
+    chi = chi_func(M_mean, M_mean_sqrd, T[np.newaxis])
     plt.figure(figsize=(12,12))
-    plt.plot(T_arr,E_mean[0,:])
+
+    plt.subplot(2,2,1)
+    plt.title(r'Energy, $\langle E \rangle$')
+    for i in range(len(L_arr)):
+        plt.plot(T_arr,E_mean[i], label='L=%i' % L_arr[i])
+    plt.ylabel(r'$\langle E \rangle$')
+    plt.legend()
+
+    plt.subplot(2,2,2)
+    plt.title(r'Magnetisation, $\langle |M| \rangle$')
+    for i in range(len(L_arr)):
+        plt.plot(T_arr,E_mean[i], label='L=%i' % L_arr[i])
+    plt.ylabel(r'$\langle E \rangle$')
+    plt.legend()
+
+    plt.subplot(2,2,3)
+    plt.title(r'Specific heat capacity, $C_v$')
+    for i in range(len(L_arr)):
+        plt.plot(T_arr,E_mean[i], label='L=%i' % L_arr[i])
+    plt.ylabel(r'$\langle E \rangle$')
+    plt.xlabel('Temperature')
+    plt.legend()
     plt.show()
+
+
+
+
+
+
     '''
     for i in range(len(L_arr)):
         plt.subplot(2,2,1)
@@ -73,6 +109,6 @@ def plot_func():
             plt.xlabel('Temperature')
             plt.legend()
     plt.show()'''
-#compile_func()
-#calc_func()
-plot_func()
+compile_func()
+calc_func()
+#plot_func()

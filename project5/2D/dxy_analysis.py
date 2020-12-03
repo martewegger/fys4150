d8df6c35e7d53877f0a4d455237c2267a02c5a69
efredborg <e.fredborg@hotmail.com>
2020-12-03 22:35:06 +0100

    torsdag

--- project5/2D/dxy_analysis.py
+++ project5/2D/dxy_analysis.py
@@ -12,12 +12,14 @@ N = 50
 dxy = np.linspace(0.005,0.2, N)
 chi_arr = np.zeros((N))
 T_end = 0.02
+r = 4
+dt = dxy[0]**2/r
 
 a=0
 for i in range(N):
     a+=1
     print('computed = ',a,'/',N, end = "\r")
-    dt = dxy[i]**2/4
+    #dt = dxy[i]**2/r
     run_func(T_end, dt, dxy[i])
     data = np.loadtxt('data_2D.txt')
     xy = np.linspace(xy0,xy1,data.shape[0])
